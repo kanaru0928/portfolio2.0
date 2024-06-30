41f1b1e2f6664be2026c41d65e675f3c79485395
kanaru0928 <kanaru0928@gmail.com>
2024-06-30 14:37:22 +0000

    add: Google Analyticsの導入 Fixes #3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [
+    react(),
+    tailwind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
