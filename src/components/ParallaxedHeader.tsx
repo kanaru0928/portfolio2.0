efddee70195e185313a6b9718eade3dcd860b2f8
kanaru0928 <kanaru0928@gmail.com>
2024-07-02 04:34:48 +0000

    change: 画像をAstroComponentにする Fixes #11

--- src/components/ParallaxedHeader.tsx
+++ src/components/ParallaxedHeader.tsx
@@ -1,25 +1,29 @@
-import React from "react";
+import React, { type ReactNode } from "react";
 import Parallax from "./Parallax";
 import useScrollPosition from "../hooks/useScrollPosition";
-import miku1 from "../images/miku1.png";
 import { TbChevronsDown } from "react-icons/tb";
+
+type ImageProps = "hero";
 
 interface ParallaxedHeaderProps {
   text: string;
   description: string;
+  children?: ReactNode;
 }
 
-export default function ParallaxedHeader({text, description}: ParallaxedHeaderProps) {
+export default function ParallaxedHeader({
+  text,
+  description,
+  children,
+}: ParallaxedHeaderProps) {
   const scrollPosition = useScrollPosition();
   const blurAmount = Math.max(Math.min((scrollPosition - 100) * 0.01, 8), 0);
 
   return (
-    <Parallax styles={{filter: `blur(${blurAmount}px)`}}>
+    <Parallax styles={{ filter: `blur(${blurAmount}px)` }}>
       <div className="hero min-h-screen relative bg-base-300">
         <div className="hero-content flex-col lg:flex-row-reverse xl:max-w-[90svw] w-full justify-center xl:justify-between">
-          <div className="w-1/2 shrink max-h-80 lg:max-h-full">
-            <img src={miku1.src} alt="Miku" />
-          </div>
+          <div className="w-1/2 shrink max-h-80 lg:max-h-full">{children}</div>
           <div className="overlay absolute top-0 bottom-0 right-0 left-0 bg-gradient-to-t from-[rgba(0,0,0,.2)] dark:from-[rgba(0,0,0,.5)]"></div>
           <div className="xl:w-1/2 shrink z-10">
             <h1 className="text-[clamp(2rem,6vw,4.2rem)] font-black text-accent">
