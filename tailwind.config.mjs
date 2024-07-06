b07f8b096b1e316521d9ad4a23940a34d36bbba4
kanaru0928 <kanaru0928@gmail.com>
2024-07-06 08:03:53 +0000

    add: skelton

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 
 import catppuccin from "@catppuccin/daisyui";
+import plugin from "tailwindcss/plugin";
 
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [
+    require("daisyui"),
+    plugin(({ addVariant, e }) => {
+      addVariant("wf-non-active", ({ modifySelectors, separator }) => {
+        modifySelectors(({ className }) => {
+          return `html:not(.wf-active) .${e(`wf-non-active${separator}${className}`)}`;
+        });
+      });
+    }),
+  ],
   daisyui: {
     themes: [catppuccin("latte", "mauve"), catppuccin("mocha", "mauve")],
     // darkTheme: "mocha",
