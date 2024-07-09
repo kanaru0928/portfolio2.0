c91c128a0432840b7b1d809057455e861e8eaea5
kanaru0928 <kanaru0928@gmail.com>
2024-07-09 04:03:33 +0000

    fix: hero headerの高さをdvhにした

--- src/components/libs/ParallaxedHeaderLegacy.tsx
+++ src/components/libs/ParallaxedHeaderLegacy.tsx
@@ -19,9 +19,9 @@ export default function ParallaxedHeader({
 
   return (
     <Parallax styles={{ filter: `blur(${blurAmount}px)` }}>
-      <div className="hero min-h-screen relative bg-base-300">
-        <div className="hero-content flex-col lg:flex-row-reverse xl:max-w-[90svw] w-full justify-center xl:justify-between">
-          <div className="w-1/2 shrink max-h-80 lg:max-h-full">{children}</div>
+      <div className="hero h-dvh relative bg-base-300">
+        <div className="hero-content flex-col lg:flex-row-reverse xl:max-w-[90svw] w-full h-dvh justify-center xl:justify-between">
+          <div className="w-1/2 shrink max-h-80 lg:max-h-full lg:h-full">{children}</div>
           <div className="overlay absolute top-0 bottom-0 right-0 left-0 bg-gradient-to-t from-[rgba(0,0,0,.2)] dark:from-[rgba(0,0,0,.5)]"></div>
           <div className="xl:w-1/2 shrink z-10">
             <div className="wf-non-active:hidden">
