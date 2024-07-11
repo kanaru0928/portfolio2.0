1158e980a795f1ea5934723e54fa1937b10bfc5f
kanaru0928 <kanaru0928@gmail.com>
2024-07-11 15:43:16 +0000

    feat: サンプル記事

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,10 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,5 +17,10 @@ export default defineConfig({
         forward: ["dataLayer.push"],
       },
     }),
+    mdx(),
   ],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
 });
